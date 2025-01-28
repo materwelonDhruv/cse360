d861465ae668ec79de3d120bfdc00edc50e1789d
materwelon <materwelondhruv@gmail.com>
2025-01-27 17:53:58 -0700

    init: project setup

---
+++ Team Project 1/src/application/StartCSE360.java
@@ -0,0 +1,32 @@
+package src.application;
+
+import java.sql.SQLException;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import src.database.DatabaseHelper;
+
+public class StartCSE360 extends Application {
+
+    private static final DatabaseHelper databaseHelper = new DatabaseHelper();
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            databaseHelper.connectToDatabase(); // Connect to the database
+            if (databaseHelper.isDatabaseEmpty()) {
+
+                new FirstPage(databaseHelper).show(primaryStage);
+            } else {
+                new SetupLoginSelectionPage(databaseHelper).show(primaryStage);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
