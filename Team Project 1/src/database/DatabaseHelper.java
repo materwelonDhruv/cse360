168bb5f2a1ed6c0138729901ce2a3e12464951d4
TheBloated1 <73143494+TheBloated1@users.noreply.github.com>
2025-02-04 14:40:30 -0700

    Commented out DB Drop

--- Team Project 1/src/database/DatabaseHelper.java
+++ Team Project 1/src/database/DatabaseHelper.java
@@ -37,7 +37,7 @@ public class DatabaseHelper {
             connection = DriverManager.getConnection(DB_URL, USER, PASS);
             statement = connection.createStatement();
             // You can use this command to clear the database and restart from fresh.
-            statement.execute("DROP ALL OBJECTS");
+            //statement.execute("DROP ALL OBJECTS");
 
             createTables(); // Create the necessary tables if they don't exist
         } catch (ClassNotFoundException e) {
