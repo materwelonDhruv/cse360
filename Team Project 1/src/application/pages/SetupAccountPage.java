92fa46dbd4f70b65c45a67192e519317591d2241
materwelon <materwelondhruv@gmail.com>
2025-02-26 13:18:14 -0700

    fix: 16 chars username

--- Team Project 1/src/application/pages/SetupAccountPage.java
+++ Team Project 1/src/application/pages/SetupAccountPage.java
@@ -31,7 +31,7 @@ public class SetupAccountPage extends BasePage {
 
         // Create input fields using UIFactory.
         TextField userNameField = UIFactory.createTextField("Enter Admin userName",
-                f -> f.maxWidth(250).minChars(16).maxChars(18));
+                f -> f.maxWidth(250).minChars(6).maxChars(18));
         TextField firstNameField = UIFactory.createTextField("Enter Admin first name",
                 f -> f.maxWidth(250).minChars(1).maxChars(50));
         TextField lastNameField = UIFactory.createTextField("Enter Admin last name",
