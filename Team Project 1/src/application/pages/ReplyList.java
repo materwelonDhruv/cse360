c561f2dc1c1d28989ac3dab812946e970b42daf1
TheBloated1 <73143494+TheBloated1@users.noreply.github.com>
2025-04-03 23:30:24 -0700

    feat: reply update
    The ReplyList class now has proper spacing for all messages

--- Team Project 1/src/application/pages/ReplyList.java
+++ Team Project 1/src/application/pages/ReplyList.java
@@ -6,22 +6,27 @@ import database.model.entities.Message;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import utils.permissions.Roles;
+
+import java.util.List;
 
 @Route(MyPages.REPLY_LIST)
 @View(title = "Reply List")
 public class ReplyList extends BasePage {
-    private static Answer parent;
+    private static Answer root;
+    private ObservableList<Answer> replies;
 
     /**
      * @param p Sets the root answer to passed parameter
      */
     public static void setAnswer(Answer p) {
-        parent = p;
+        root = p;
     }
 
     /**
@@ -41,18 +46,36 @@ public class ReplyList extends BasePage {
         bottomBar.getChildren().addAll(backButton, logoutButton);
         layout.setBottom(bottomBar);
         //Setup ListView of replies
-        ListView<Answer> replyList = new ListView<>();
-        ObservableList<Answer> replies = FXCollections.observableArrayList(context.answers().getRepliesToAnswer(parent.getId()));
-        replyList.setItems(replies);
+        ListView<Answer> replyList = replyListViewSetup();
         layout.setCenter(replyList);
         //Setup top toolbar
         TextField replyInput = UIFactory.createTextField("Reply:", f -> f.minWidth(200).maxWidth(600).minChars(10).maxChars(2000));
         Button addReply = replyAddButtonSetup(replyList, replyInput);
         Button editReply = replyEditButtonSetup();
         Button deleteReply = replyDeleteButtonSetup(replyList.getSelectionModel().getSelectedItem());
-        topBar.getChildren().addAll(replyInput, addReply, editReply, deleteReply);
+        Button addReplyToSelected = replyToSelectedButtonSetup(replyList, replyInput);
+        topBar.getChildren().addAll(replyInput, addReply, addReplyToSelected, editReply, deleteReply);
         layout.setTop(topBar);
         return layout;
+    }
+
+
+    private ListView<Answer> replyListViewSetup() {
+        ListView<Answer> replyList = new ListView<>();
+        updateList();
+        replyList.setItems(replies);
+        replyList.setCellFactory(param -> new ListCell<Answer>() {
+            @Override
+            protected void updateItem(Answer item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty || item == null) {
+                    setText(null);
+                } else {
+                    setText(item.getMessage().getContent());
+                }
+            }
+        });
+        return replyList;
     }
 
     /**
@@ -66,9 +89,34 @@ public class ReplyList extends BasePage {
                 e -> e.onAction(
                         a -> {
                             String replyContent = replyInput.getCharacters().toString();
-                            Message tempMessage = new Message(parent.getMessage().getUserId(), replyContent);
-                            Answer newAnswer = new Answer(tempMessage, null, parent.getId(), false);
+                            if (context.getSession().getCurrentRole() == Roles.REVIEWER) {
+                                replyContent = "φ " + replyContent + " φ";
+                            }
+                            Message tempMessage = new Message(context.getSession().getActiveUser().getId(), replyContent);
+                            Answer newAnswer = new Answer(tempMessage, null, root.getId(), false);
                             context.answers().create(newAnswer);
+                            updateList();
+                            replyTable.setItems(replies);
+                        }
+                )
+        );
+        return addReplyButton;
+    }
+
+    private Button replyToSelectedButtonSetup(ListView<Answer> replyTable, TextField replyInput) {
+        Button addReplyButton = UIFactory.createButton("Add Reply to Selected",
+                e -> e.onAction(
+                        a -> {
+                            String replyContent = replyInput.getCharacters().toString();
+                            if (context.getSession().getCurrentRole() == Roles.REVIEWER) {
+                                replyContent = "φ " + replyContent + " φ";
+                            }
+                            Answer parentAnswer = replyTable.getSelectionModel().getSelectedItem();
+                            Message tempMessage = new Message(context.getSession().getActiveUser().getId(), replyContent);
+                            Answer newAnswer = new Answer(tempMessage, null, parentAnswer.getId(), false);
+                            context.answers().create(newAnswer);
+                            updateList();
+                            replyTable.setItems(replies);
                         }
                 )
         );
@@ -98,5 +146,47 @@ public class ReplyList extends BasePage {
                 )
         );
         return deleteReplyButton;
+    }
+
+    private void updateList() {
+        ObservableList<Answer> tempReplyList = FXCollections.observableArrayList();
+        replies = findAllAnswers(root, tempReplyList);
+        rearrangeAnswers(replies);
+    }
+
+    private ObservableList<Answer> findAllAnswers(Answer answer, ObservableList<Answer> tempReplies) {
+        List<Answer> localReplies = context.answers().getRepliesToAnswer(answer.getId());
+        tempReplies.add(answer);
+        if (localReplies == null) {
+            return tempReplies;
+        }
+        for (Answer reply : localReplies) {
+            findAllAnswers(reply, tempReplies);
+        }
+        return tempReplies;
+    }
+
+    private void rearrangeAnswers(ObservableList<Answer> replies) {
+        resetSpacing();
+        for (Answer reply : replies) {
+            int depth = 0;
+            Answer tempAnswer = reply;
+            while (tempAnswer != root && tempAnswer.getParentAnswerId() != null) {
+                depth++;
+                tempAnswer = context.answers().getById(tempAnswer.getParentAnswerId());
+            }
+            StringBuilder reformat = new StringBuilder(reply.getMessage().getContent());
+            for (int i = 0; i < depth; i++) {
+                reformat.insert(0, "    ");
+            }
+            reply.getMessage().setContent(reformat.toString());
+        }
+    }
+
+    private void resetSpacing() {
+        for (Answer reply : replies) {
+            String fixSpacing = reply.getMessage().getContent().strip();
+            reply.getMessage().setContent(fixSpacing);
+        }
     }
 }
