414b4374fa80fd484d9f2421364e3e19f71b7348
TheBloated1 <73143494+TheBloated1@users.noreply.github.com>
2025-04-23 18:30:12 -0700

    feat: basic UI setup for Solved
    setup PendingAdminRequests

--- Team Project 1/src/application/pages/admin/PendingAdminRequests.java
+++ Team Project 1/src/application/pages/admin/PendingAdminRequests.java
@@ -1,14 +1,52 @@
 package application.pages.admin;
 
-import application.framework.MyPages;
-import application.framework.Route;
-import application.framework.View;
+import application.framework.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import utils.permissions.Roles;
 
 /**
  *
  */
 @Route(MyPages.ADMIN_PENDING)
 @View(title = "Pending Admin Requests")
-public class PendingAdminRequests {
+public class PendingAdminRequests extends BasePage {
+    private final Roles currentRole = context.getSession().getCurrentRole();
+
+    public Pane createView() {
+        VBox layout = new VBox(10);
+        layout.setStyle(DesignGuide.MAIN_PADDING + " " + DesignGuide.CENTER_ALIGN);
+        Label title = new Label("Pending Admin Requests");
+        ListView<VBox> pendingAdminRequests = setupPendingRequests();
+        layout.getChildren().addAll(title, pendingAdminRequests);
+        return layout;
+    }
+
+    private ListView<VBox> setupPendingRequests() {
+        ListView<VBox> tempView = new ListView<>();
+        VBox infoVBox = new VBox(10);
+        //TODO: add the title/description of the request from the DB
+        VBox buttonVBox = new VBox(10);
+
+        if (currentRole == Roles.ADMIN) {
+            Button acceptButton = setupAcceptButton();
+            Button rejectButton = setupRejectButton();
+            buttonVBox.getChildren().addAll(acceptButton, rejectButton);
+        }
+        return tempView;
+    }
+
+    private Button setupAcceptButton() {
+        Button acceptButton = new Button("Accept");
+        return acceptButton;
+    }
+
+    private Button setupRejectButton() {
+        Button rejectButton = new Button("Reject");
+        return rejectButton;
+    }
 
 }
--- Team Project 1/src/application/pages/admin/SolvedAdminRequests.java
+++ Team Project 1/src/application/pages/admin/SolvedAdminRequests.java
@@ -1,13 +1,19 @@
 package application.pages.admin;
 
+import application.framework.BasePage;
 import application.framework.MyPages;
 import application.framework.Route;
 import application.framework.View;
+import javafx.scene.layout.Pane;
 
 /**
  *
  */
 @Route(MyPages.ADMIN_SOLVED)
-@View(title = "Pending Admin Requests")
-public class SolvedAdminRequests {
+@View(title = "Solved Admin Requests")
+public class SolvedAdminRequests extends BasePage {
+    @Override
+    public Pane createView() {
+        return null;
+    }
 }
