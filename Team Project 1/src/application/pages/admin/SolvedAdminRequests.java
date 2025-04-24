e53a286ae6ebc33ca25923823d111990b8e27caa
92ndChicken <tyler.j.woodburn@gmail.com>
2025-04-23 18:22:44 -0700

    feat: basic ui set up for solved admin requests page

--- Team Project 1/src/application/pages/admin/SolvedAdminRequests.java
+++ Team Project 1/src/application/pages/admin/SolvedAdminRequests.java
@@ -1,13 +1,39 @@
 package application.pages.admin;
 
-import application.framework.MyPages;
-import application.framework.Route;
-import application.framework.View;
+import application.framework.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 /**
+ *  Displays the list of all closed/solved admin requests and allows Instructors
+ *  to reopen a closed admin request and update its description.
  *
+ * @author Tyler
  */
 @Route(MyPages.ADMIN_SOLVED)
-@View(title = "Pending Admin Requests")
-public class SolvedAdminRequests {
+@View(title = "Solved Admin Requests")
+public class SolvedAdminRequests extends BasePage {
+    /**
+     * Creates the layout for the SolvedAdminRequests Page.
+     * @return A Pane containing the layout of the page.
+     */
+    @Override
+    public Pane createView() {
+        VBox layout = new VBox(15);
+        layout.setStyle(DesignGuide.MAIN_PADDING + " " + DesignGuide.CENTER_ALIGN);
+
+        Label titleLabel = UIFactory.createLabel("Solved Admin Requests");
+
+        // ListView to display the solved requests
+        ListView<String> requestView = new ListView<>();
+
+        // Button to navigate to the previous page
+        Button backButton = UIFactory.createBackButton(context);
+
+        layout.getChildren().addAll(titleLabel, requestView, backButton);
+        return layout;
+    }
 }
