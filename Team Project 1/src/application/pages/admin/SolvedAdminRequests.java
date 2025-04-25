cf0fcb0c4970be7d79c435ca56f50c9330939641
92ndChicken <tyler.j.woodburn@gmail.com>
2025-04-25 01:38:16 -0700

    feat: in progress solved admin requests page

--- Team Project 1/src/application/pages/admin/SolvedAdminRequests.java
+++ Team Project 1/src/application/pages/admin/SolvedAdminRequests.java
@@ -4,6 +4,7 @@ import application.framework.BasePage;
 import application.framework.MyPages;
 import application.framework.Route;
 import application.framework.View;
+import database.model.entities.AdminRequest;
 import javafx.scene.layout.Pane;
 import application.framework.*;
 import javafx.scene.control.Button;
@@ -11,6 +12,10 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.util.Pair;
+import utils.requests.RequestState;
+
+import java.util.List;
 
 /**
  *  Displays the list of all closed/solved admin requests and allows Instructors
@@ -21,11 +26,9 @@ import javafx.scene.layout.VBox;
 @Route(MyPages.ADMIN_SOLVED)
 @View(title = "Solved Admin Requests")
 public class SolvedAdminRequests extends BasePage {
-    @Override
-    public Pane createView() {
-        return null;
-    }
-}
+    // ListView containing all solved admin requests
+    private final ListView<Pair<Integer,VBox>> requestView = new ListView<>();
+
     /**
      * Creates the layout for the SolvedAdminRequests Page.
      * @return A Pane containing the layout of the page.
@@ -37,13 +40,49 @@ public class SolvedAdminRequests extends BasePage {
 
         Label titleLabel = UIFactory.createLabel("Solved Admin Requests");
 
-        // ListView to display the solved requests
-        ListView<String> requestView = new ListView<>();
+        loadSolvedRequests();
+
+        // Button to reopen the selected admin request
+        Button reopenRequestButton = UIFactory.createButton("Reopen Selected Request", e -> e.onAction(a -> {
+            Pair<Integer,VBox> selectedRequest = requestView.getSelectionModel().getSelectedItem();
+            if (selectedRequest != null) {
+                //TODO: navigate to usermodifypage?
+            }
+        }));
 
         // Button to navigate to the previous page
         Button backButton = UIFactory.createBackButton(context);
 
-        layout.getChildren().addAll(titleLabel, requestView, backButton);
+        layout.getChildren().addAll(titleLabel, requestView, reopenRequestButton, backButton);
         return layout;
+    }
+
+    /**
+     * Loads all solved requests from the database into the requestView
+     * sorted by newest to oldest.
+     */
+    private void loadSolvedRequests() {
+        requestView.getItems().clear();
+        List<AdminRequest> solvedRequests = context.adminRequests().filterFetch(RequestState.Accepted);
+        for (AdminRequest request : solvedRequests) {
+            requestView.getItems().addFirst(new Pair<>(request.getId(), createRequestVBox(request)));
+        }
+    }
+
+    /**
+     * Creates a VBox containing the title and description of the given request
+     *
+     * @param request the {@link AdminRequest} to create a VBox for
+     * @return the VBox containing the request information
+     */
+    private VBox createRequestVBox(AdminRequest request) {
+        // Label for the Request's title
+        //Label titleLabel = new Label(request.getTitle() + " | " + Helpers.formatTimestamp(announcement.getMessage().getCreatedAt()));
+        //titleLabel.setStyle("-fx-font-weight: bold");
+
+        // Label for the announcement's content
+        //Label contentLabel = new Label(announcement.getMessage().getContent());
+
+        return new VBox();
     }
 }
